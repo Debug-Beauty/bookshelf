f53834d9495ff55531f4a86e59321a24b6dc76c8
TalitaCaetano <guimaraestalita19@gmail.com>
2025-09-24 22:08:50 -0300

    correção de erro

--- app/api/books/[id]/route.ts
+++ app/api/books/[id]/route.ts
@@ -1,51 +1,28 @@
 import { NextResponse } from 'next/server';
-import { books } from '../route'; 
+import { books } from '@/lib/inMemoryStore'; 
 
-export async function GET(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function GET(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
   const book = books.find(b => b.id === id);
-
-  if (!book) {
-    return NextResponse.json({ error: 'Livro não encontrado' }, { status: 404 });
-  }
-
+  if (!book) return NextResponse.json({ error: 'Livro não encontrado' }, { status: 404 });
   return NextResponse.json(book);
 }
 
-export async function PUT(
-  request: Request,
-  { params }: { params: { id: string } }
-) {
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
   const bookIndex = books.findIndex(b => b.id === id);
-
-  if (bookIndex === -1) {
-    return NextResponse.json({ error: 'Livro não encontrado' }, { status: 404 });
-  }
-
+  if (bookIndex === -1) return NextResponse.json({ error: 'Livro não encontrado' }, { status: 404 });
+  
   const updatedBookData = await request.json();
-
-
   books[bookIndex] = { ...books[bookIndex], ...updatedBookData };
-
   return NextResponse.json(books[bookIndex]);
 }
 
-export async function DELETE(
-  request: Request,
-  { params }: {params: {id: string} }
-){
+export async function DELETE(request: Request, { params }: { params: { id: string } }) {
   const { id } = params;
   const bookIndex = books.findIndex(b => b.id === id);
+  if (bookIndex === -1) return NextResponse.json({ error: 'Livro não encontrado' }, { status: 404 });
 
-   if ( bookIndex === -1) {
-    return NextResponse.json({ error: 'Livro não encontrado' }, {status: 404 });
-   }
-
-   books.splice(bookIndex, 1);
-
-   return NextResponse.json({ message: 'Livro removido com sucesso' });
+  books.splice(bookIndex, 1);
+  return NextResponse.json({ message: 'Livro removido com sucesso' });
 }
