83b4265df6a29072fa6694f013aab3fe2bedda4c
Ewerton Hecsley <ewerton.martinscomercial@gmail.com>
2025-10-05 16:10:08 -0300

    refactor: implementation prisma in action auth

--- app/(auth)/register/actions.ts
+++ app/(auth)/register/actions.ts
@@ -8,21 +8,21 @@ export interface RegisterState {
   message: string;
 }
 
-export async function registerUser(prevState: RegisterState, formData: FormData): Promise<RegisterState> {
+export async function registerUser(
+  state: void | RegisterState,
+  formData: FormData
+): Promise<RegisterState | void> {
   const email = formData.get('email') as string;
   const password = formData.get('password') as string;
   const confirmPassword = formData.get('confirmPassword') as string;
 
   const validationError = validateRegistrationForm({ email, password, confirmPassword });
-  if (validationError) {
-    return validationError;
-  }
+  if (validationError) return validationError;
 
   try {
     await addUser({ email, password });
+    redirect('/login');
   } catch (error: any) {
     return { message: error.message };
   }
-
-  redirect('/login');
 }
